d7ed31f7d9dc63378f88cb64a70f5da07e8b2892
yzbai <yzbai@brlite.com>
2017-07-06 20:26:44 +0800

    Add project files.

---
+++ AliyunSmsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AliyunSmsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
