38a19a81256c8e6191f456998e871fa6f1d200ed
yzbai <yzbai@brlite.com>
2017-07-06 20:49:41 +0800

    demo finished.

--- AliyunSmsDemo/Program.cs
+++ AliyunSmsDemo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Sms.Model.V20170525;
+using System;
 
 namespace AliyunSmsDemo
 {
@@ -6,7 +9,36 @@ namespace AliyunSmsDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //以下字段换成自己的
+            string regionId = "cn-hangzhou";
+            string accessKeyId = "xxxxxxxx";
+            string accessKeySecret = "xxxxxxxxxxxxxxxxxxxxxxxx";
+            string signName = "xxxxxxxxxx";
+            string phoneNumbers = "15xxxxxxxxx";
+            string templateCode = "SMS_73985016";
+            string templateParam = "{\"code\":\"123456\", \"product\":\"MyProduct\"}";
+
+
+            IClientProfile clientProfile = DefaultProfile.GetProfile(regionId, accessKeyId, accessKeySecret);
+            DefaultProfile.AddEndpoint(regionId, regionId, "Dysmsapi", "dysmsapi.aliyuncs.com");
+
+            IAcsClient acsClient = new DefaultAcsClient(clientProfile);
+
+            SendSmsRequest request = new SendSmsRequest();
+
+            request.SignName = signName;
+            request.TemplateCode = templateCode;
+            request.PhoneNumbers = phoneNumbers;
+            request.TemplateParam = templateParam;
+
+            try
+            {
+                SendSmsResponse response = acsClient.GetAcsResponse(request);
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
